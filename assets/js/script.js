a080807e0b4614b8df76114a5c7cf5cebc104e28
Davej66 <daijacobs@gmail.com>
2021-08-11 09:11:33 +0000

    Set up structure of game functions.

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function runGame(){
+
+}
+
+function checkAnswer() {
+
+}
+
+function  checkCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongScore() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySuntractionQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
